89771bf2a4f6841cf2ff16cede7b58c15b2ffa70
dima <dda.dream@gmail.com>
2024-05-23 09:54:12 +0300

    publish problem FIX

--- WinFormsApp1/PUBLISH.cmd
+++ WinFormsApp1/PUBLISH.cmd
@@ -1,2 +1,3 @@
-﻿
-dotnet publish --ucr
+﻿rem if no publish DO THIS
+rem goto C:\Users\username\AppData\Local\Microsoft\VisualStudio\17.0_xxxxxxxx folder and rename/remove the ComponentModelCache folder.
+dotnet publish --ucr -o c:\NAPOMINATOR
