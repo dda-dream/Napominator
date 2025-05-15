8445a99eae0c747dd68bd988d5c3f39b7829f0ef
Dmitry Dmitry <dda.dream@gmail.com>
2025-05-15 14:45:40 +0300

    #

--- WinFormsApp1/PUBLISH.cmd
+++
@@ -1,3 +0,0 @@
-﻿rem if no publish DO THIS
-rem goto C:\Users\username\AppData\Local\Microsoft\VisualStudio\17.0_xxxxxxxx folder and rename/remove the ComponentModelCache folder.
-dotnet publish --ucr -o c:\NAPOMINATOR
