7d60ba8bb8f144ab9b2ac91ac524e5e3af0e5c82
Dmitry Dmitry-asus zenbook <dda-dream@users.noreply.github.com>
2025-10-03 09:07:42 +0300

    #

--- WinFormsApp1/MainForm.cs
+++ WinFormsApp1/MainForm.cs
@@ -325,7 +325,7 @@ namespace Napominator
                 timer1.Start();
                 button_StartStop.Text = "Stop";
 
-                if (USERNAME != "d")
+                if (USERNAME != "d" && USERNAME != "r")
                     DisableControls(this);
 
                 notifyIcon1.Icon = Napominator.Properties.Resources.Icon1;
